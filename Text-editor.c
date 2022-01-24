2d4bef8c6435a4939f00d96db6e359fa942cf3c0
Ioannis Kozyris <80053394+ikozyris@users.noreply.github.com>
2022-01-24 12:11:27 +0200

    v0.3.1 update

--- Text-editor.c
+++ Text-editor.c
@@ -9,7 +9,6 @@ int main()
     FILE* fp;
     int m, n;
     char fname[12];
-    char wname[12];
 
 
     printf("Text editor made by ikozyris and gkozyris\nChoose between reading-1 overwriting-2 writing-3: ");
@@ -18,14 +17,14 @@ int main()
 
     if (m == 2)
     {
-        printf("Instructions below:\nuse_for space\nuse up to 30 characters\n\nEnter text: ");
+        printf("Instructions below:\nUse_for space\nUse up to 30 characters\nUse up to 12 characters for file name\n\nEnter text: ");
         scanf("%s", txt); //sets char-txt to the entered text
         printf("%s\n", txt);
         //naming file
         printf("Enter file name to write: ");
-        scanf("%s", &wname);
+        scanf("%s", &fname);
         //creates file
-        fp = fopen(wname, "w+");
+        fp = fopen(fname, "w+");
         fputs(txt, fp); //creates file
         fclose(fp); //closes file
         return 0;
@@ -37,15 +36,15 @@ int main()
         scanf("%s", &fname);
         printf("text contains:\n");
         fp = fopen(fname,"r");
-        fscanf(fp,"%s",txt);
+        fscanf(fp,"%s",txt); //reading file content
         printf("%s\n",txt);
-        fclose(fp);
+        fclose(fp); //closing file
         return 0;
     }
 
     else if (m == 3)
     {
-        printf("Instructions below:\nuse_for space\nuse up to 30 characters\n\nEnter text: ");
+        printf("Instructions below:\nUse_for space\nUse up to 30 characters\nUse up to 12 characters for file name\n\nEnter text: ");
         scanf("%s", txt); //sets char-txt to the entered text
             //naming file
         printf("Enter file name to write: ");
@@ -60,7 +59,7 @@ int main()
 
     else
     {
-        printf("Invalid option use 1 or 2 or 3\n");
+        printf("Invalid option :( use 1 or 2 or 3\n");
         return 0;
     }
 }
