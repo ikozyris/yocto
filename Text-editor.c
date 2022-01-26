e7334cc3c72a5f44e1ab3dfdeb0ef18f2cb16511
Ioannis Kozyris <80053394+ikozyris@users.noreply.github.com>
2022-01-26 12:55:09 +0200

    Optimized
    Deleted unnecessary library and added more comments.

--- Text-editor.c
+++ Text-editor.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
-
 {
     //declaring
     char txt[30];
     FILE* fp;
     int m, n;
     char fname[12];
-
-
-    printf("Text editor made by ikozyris and gkozyris\nChoose between reading-1 overwriting-2 writing-3: ");
+    
+    printf("Text editor made by ikozyris and gkozyris\nChoose between reading-1 overwriting-2 editing-3: ");
     scanf("%d",&m);
     printf("%d\n",m);
 
-    if (m == 2)
+    if (m == 2) //overwrite
     {
         printf("Instructions below:\nUse_for space\nUse up to 30 characters\nUse up to 12 characters for file name\n\nEnter text: ");
         scanf("%s", txt); //sets char-txt to the entered text
@@ -29,8 +26,7 @@ int main()
         fclose(fp); //closes file
         return 0;
     }
-
-    else if (m == 1)
+    else if (m == 1) //reading
     {
         printf("Enter file name to read: ");
         scanf("%s", &fname);
@@ -41,8 +37,7 @@ int main()
         fclose(fp); //closing file
         return 0;
     }
-
-    else if (m == 3)
+    else if (m == 3) //edit
     {
         printf("Instructions below:\nUse_for space\nUse up to 30 characters\nUse up to 12 characters for file name\n\nEnter text: ");
         scanf("%s", txt); //sets char-txt to the entered text
@@ -50,16 +45,15 @@ int main()
         printf("Enter file name to write: ");
         scanf("%s", &fname);
         //processing file
-        fp = fopen(fname,"a"); //opens file in write mode
+        fp = fopen(fname,"a"); //opens file in edit mode
         fprintf(fp,"%s",txt); //writes to file without overwriting
         fclose(fp); //closes file
         printf("%s\n", txt,"%s",n);
         return 0;
     }
-
     else
     {
-        printf("Invalid option :( use 1 or 2 or 3\n");
+        printf("Invalid option: %d :( use 1 or 2 or 3\n",m);
         return 0;
     }
 }
