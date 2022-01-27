0eece708e395d1615b16b14dc414d36d7483f2a6
Ioannis Kozyris <80053394+ikozyris@users.noreply.github.com>
2022-01-27 21:14:49 +0200

    v0.4
    Big thanks to @gkozyris.

--- Text-editor.c
+++ Text-editor.c
@@ -5,55 +5,70 @@ int main()
     //declaring
     char txt[30];
     FILE* fp;
-    int m, n;
-    char fname[12];
-    
-    printf("Text editor made by ikozyris and gkozyris\nChoose between reading-1 overwriting-2 editing-3: ");
-    scanf("%d",&m);
-    printf("%d\n",m);
+    int m, i, lines=0;
+    char fname[12],ch;
 
-    if (m == 2) //overwrite
+    printf("Text editor made by ikozyris and gkozyris");
+    while(1)
     {
-        printf("Instructions below:\nUse_for space\nUse up to 30 characters\nUse up to 12 characters for file name\n\nEnter text: ");
-        scanf("%s", txt); //sets char-txt to the entered text
-        printf("%s\n", txt);
-        //naming file
-        printf("Enter file name to write: ");
-        scanf("%s", &fname);
-        //creates file
-        fp = fopen(fname, "w+");
-        fputs(txt, fp); //creates file
-        fclose(fp); //closes file
-        return 0;
-    }
-    else if (m == 1) //reading
-    {
-        printf("Enter file name to read: ");
-        scanf("%s", &fname);
-        printf("text contains:\n");
-        fp = fopen(fname,"r");
-        fscanf(fp,"%s",txt); //reading file content
-        printf("%s\n",txt);
-        fclose(fp); //closing file
-        return 0;
-    }
-    else if (m == 3) //edit
-    {
-        printf("Instructions below:\nUse_for space\nUse up to 30 characters\nUse up to 12 characters for file name\n\nEnter text: ");
-        scanf("%s", txt); //sets char-txt to the entered text
-            //naming file
-        printf("Enter file name to write: ");
-        scanf("%s", &fname);
-        //processing file
-        fp = fopen(fname,"a"); //opens file in edit mode
-        fprintf(fp,"%s",txt); //writes to file without overwriting
-        fclose(fp); //closes file
-        printf("%s\n", txt,"%s",n);
-        return 0;
-    }
-    else
-    {
-        printf("Invalid option: %d :( use 1 or 2 or 3\n",m);
-        return 0;
+        printf("\nChoose between reading-1 overwriting-2 writing-3 quit-4: ");
+        scanf("%d",&m);
+
+        if (m == 2)
+        {
+            printf("Instructions below:\nUse_for space\nUse up to 30 characters\nUse up to 12 characters for file name\n\nEnter text: ");
+            scanf("%s", txt); //sets char-txt to the entered text
+            printf("%s\n", txt);
+            printf("Enter file name to write: ");
+            scanf("%s", &fname); //naming the file
+            fp = fopen(fname, "w");
+            fputs(txt, fp); //creates file
+            fclose(fp); //closes file
+        }
+
+        else if (m == 1)
+        {
+            printf("Enter file name to read: ");
+            scanf("%s", &fname);
+            fp = fopen(fname,"r");
+
+            while((ch=fgetc(fp))!=EOF)//getting number of lines (works)
+            {
+                if(ch=='\n')    //but destroys the fscanf
+                lines++;
+            }
+            fclose(fp);
+            fp = fopen(fname,"r");
+            printf("\nlines:%d\n", lines);
+            fscanf(fp,"%[^\n]",txt);
+            printf("text contains:\n%s\n",txt);
+            for (i = 0; i < lines; i++)
+            {
+                fscanf(fp,"\n%[^\n]\n",txt);
+                printf("%s\n",txt);
+            }
+            fclose(fp);
+        }
+
+        else if (m == 3)
+        {
+            printf("Instructions below:\nUse_for space\nUse up to 30 characters for file content\nUse up to 12 characters for file name\n\nEnter text: ");
+            scanf("%s", txt); //sets char-txt to the entered text
+                //naming file
+            printf("Enter file name to write: ");
+            scanf("%s", &fname);
+                //processing file
+            fp = fopen(fname,"a"); //opens file in write mode and enters text in the end of the line
+            fprintf(fp,"%s",txt); //writes to file without overwriting
+            fclose(fp); //closes file
+            printf("%s\n", txt);
+        }
+        else if (m == 4)
+            return 0;
+
+        else
+        {
+            printf("Invalid option: %d :( use 1 or 2 or 3 or 4\n", m);
+        }
     }
 }
