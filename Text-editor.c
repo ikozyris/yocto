5f53e4cb3732b39d9101f7704bd6d1d3f48d5929
Ioannis Kozyris <80053394+ikozyris@users.noreply.github.com>
2022-04-18 20:53:53 +0300

    Fix #2
    Note that it does work but needs double space on mode selector

--- Text-editor.c
+++ Text-editor.c
@@ -16,26 +16,68 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 */
+
 #include <stdio.h>
+
 
 int main()
 {
     //declaring
-    char txt[30];
+
+    char txt[100];
     FILE* fp;
     int m, i, lines=0;
-    char fname[12],ch;
+    char fname[20],ch;
 
     printf("Text editor made by ikozyris and gkozyris");
     while(1)
     {
+////////MODE-SELECTOR////////////////////        
         printf("\nChoose between reading-1 overwriting-2 writing-3 quit-4: ");
         scanf("%d",&m);
+        while ((getchar()) != '\n');
+        ch = getchar();
 
-        if (m == 2)
+///////READING/////////////////////////////////
+        if (m == 1)
+        {
+            printf("Enter file name to read: ");
+            scanf("%19s", fname);
+            fp = fopen(fname,"r");
+            if(fp  == NULL)
+            {
+                fclose(fp);
+                return 0;
+            }
+
+            else
+            {
+                while((ch=fgetc(fp))!=EOF)//getting number of lines
+                {
+                    if(ch=='\n')
+                    lines++;
+                }
+                fclose(fp);
+                fp = fopen(fname,"r"); //open file in read mode
+                printf("\nlines:%d\n", lines);
+            // getline (cin, text, '\n');
+                fscanf(fp,"%[^\n]",txt); //scan untill 'newline' character
+                printf("text contains:\n%s\n",txt);
+                for (i = 2; i < lines; i++)
+                {
+                // getline (cin, text, '\n');
+                    fscanf(fp,"\n%[^\n]\n",txt);
+                    printf("%s\n",txt);
+                }
+                fclose(fp);
+            }
+        }
+
+//////OVERWRITING/////////////////////////////////
+        else if (m == 2)
         {
             printf("Instructions below:\nUse_for space\nUse up to 30 characters\nUse up to 12 characters for file name\n\nEnter text: ");
-            scanf("%s", txt); //sets char-txt to the entered text
+            scanf("%[^\n]", txt); //sets char-txt to the entered text
             printf("%s\n", txt);
             printf("Enter file name to write: ");
             scanf("%s", &fname); //naming the file
@@ -44,43 +86,21 @@ int main()
             fclose(fp); //closes file
         }
 
-        else if (m == 1)
-        {
-            printf("Enter file name to read: ");
-            scanf("%s", &fname);
-            fp = fopen(fname,"r");
-
-            while((ch=fgetc(fp))!=EOF)//getting number of lines (works)
-            {
-                if(ch=='\n')    
-                lines++;
-            }
-            fclose(fp);
-            fp = fopen(fname,"r");
-            printf("\nlines:%d\n", lines);
-            fscanf(fp,"%[^\n]",txt);
-            printf("text contains:\n%s\n",txt);
-            for (i = 0; i < lines; i++)
-            {
-                fscanf(fp,"\n%[^\n]\n",txt);
-                printf("%s\n",txt);
-            }
-            fclose(fp);
-        }
-
+////////WRITING/////////////////////////////////
         else if (m == 3)
         {
             printf("Instructions below:\nUse_for space\nUse up to 30 characters for file content\nUse up to 12 characters for file name\n\nEnter text: ");
-            scanf("%s", txt); //sets char-txt to the entered text
+            scanf("%29s", txt); //sets char txt to the entered text
                 //naming file
             printf("Enter file name to write: ");
-            scanf("%s", &fname);
+            scanf("%19s", fname);
                 //processing file
             fp = fopen(fname,"a"); //opens file in write mode and enters text in the end of the line
-            fprintf(fp,"%s",txt); //writes to file without overwriting
+            fprintf(fp,"%29s",txt); //writes to file without overwriting
             fclose(fp); //closes file
-            printf("%s\n", txt);
+            printf("%s29s", txt);
         }
+////////EXITING/////////////////////////////////
         else if (m == 4)
             return 0;
 
