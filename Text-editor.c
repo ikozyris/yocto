b3fb168e73986bfffb77adb6300a0bb1cf374998
Ioannis Kozyris <80053394+ikozyris@users.noreply.github.com>
2022-05-02 00:09:20 +0300

    Fixed #1 completly
    The program was reading characters as strings, now using getch instead of fscanf fixed it.

--- Text-editor.c
+++ Text-editor.c
@@ -1,23 +1,78 @@
+/*
+    Text Editor
+    Copyright (C) 2021 - 2022  Ioannis Kozyris
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
+
 
 int main()
 {
     //declaring
-    char txt[30];
+
+    char txt[100];
     FILE* fp;
-    int m, i, lines=0;
-    char fname[12],ch;
+    int m, i, g, ch, lines=0;
+    char fname[20];
 
     printf("Text editor made by ikozyris and gkozyris");
     while(1)
     {
+////////MODE-SELECTOR////////////////////
+        lines=0;
         printf("\nChoose between reading-1 overwriting-2 writing-3 quit-4: ");
         scanf("%d",&m);
+        while ((getchar()) != '\n');
+        ch = getchar();
 
-        if (m == 2)
+///////READING/////////////////////////////////
+        if (m == 1)
+        {
+            printf("Enter file name to read: ");
+            scanf("%19s", fname);
+            fp = fopen(fname,"r");
+            if (fp  == NULL)
+            {
+                fclose(fp);
+                return 0;
+            }
+
+            else
+            {
+                while((ch=fgetc(fp))!=EOF)//getting number of lines
+                {
+                    if(ch=='\n')
+                    lines++;
+                }
+                fclose(fp);
+                fp = fopen(fname,"r"); //open file in read mode
+                printf("\nlines:%d\n", lines);
+                printf("text contains:\n%s\n",txt);
+                for (i = 2; i < lines; i++)
+                {
+                    while ((ch = getc(fp)) != EOF)
+                        putc(ch, stdout);
+                    printf("%c",txt);
+                }
+                fclose(fp);
+            }
+        }
+
+//////OVERWRITING/////////////////////////////////
+        else if (m == 2)
         {
             printf("Instructions below:\nUse_for space\nUse up to 30 characters\nUse up to 12 characters for file name\n\nEnter text: ");
-            scanf("%s", txt); //sets char-txt to the entered text
+            scanf("%[^\n]", txt); //sets char-txt to the entered text
             printf("%s\n", txt);
             printf("Enter file name to write: ");
             scanf("%s", &fname); //naming the file
@@ -26,43 +81,19 @@ int main()
             fclose(fp); //closes file
         }
 
-        else if (m == 1)
-        {
-            printf("Enter file name to read: ");
-            scanf("%s", &fname);
-            fp = fopen(fname,"r");
-
-            while((ch=fgetc(fp))!=EOF)//getting number of lines (works)
-            {
-                if(ch=='\n')
-                lines++;
-            }
-            fclose(fp);
-            fp = fopen(fname,"r");
-            printf("\nlines:%d\n", lines);
-            fscanf(fp,"%[^\n]",txt);
-            printf("text contains:\n%s\n",txt);
-            for (i = 1; i < lines; i++)
-            {
-                fscanf(fp,"\n%[^\n]\n",txt);
-                printf("%s\n",txt);
-            }
-            fclose(fp);
-        }
-
+////////WRITING/////////////////////////////////
         else if (m == 3)
         {
-            printf("Instructions below:\nUse_for space\nUse up to 30 characters for file content\nUse up to 12 characters for file name\n\nEnter text: ");
-            scanf("%s", txt); //sets char-txt to the entered text
-                //naming file
-            printf("Enter file name to write: ");
-            scanf("%s", &fname);
-                //processing file
-            fp = fopen(fname,"a"); //opens file in write mode and enters text in the end of the line
-            fprintf(fp,"%s",txt); //writes to file
-            fclose(fp); //closes file
+            printf("Instructions below:\nUse_for space\nUse up to 30 characters\nUse up to 12 characters for file name\n\nEnter text: ");
+            scanf("%[^\n]", txt); //sets char-txt to the entered text
             printf("%s\n", txt);
+            printf("Enter file name to write: ");
+            scanf("%s", &fname); //naming the file
+            fp = fopen(fname, "a");
+            fputs(txt, fp); //creates file
+            fclose(fp); //closes file
         }
+////////EXITING/////////////////////////////////
         else if (m == 4)
             return 0;
 
