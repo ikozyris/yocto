4c145004cdc0473044c4b8caad9bd2c9f8b0afa1
ikozyris <80053394+ikozyris@users.noreply.github.com>
2022-12-24 16:58:58 +0200

    No longer outputs control keys + ^E works
    Note that you can no longer write non-ascii characters

--- main.c
+++ main.c
@@ -1,94 +1,98 @@
 #include <ncurses.h>
-#include <string.h>
+//#include <string.h>
+//#include <unistd.h>
+//#include <locale.h>
 
 #define ctrl(x)           ((x) & 0x1f)
 
 int main() {
     int y, x;
-    int maxy,maxx;				// to store the maximum rows and columns
-    int ch, i, crow;
+    int maxy,maxx; // to store the maximum rows and columns
+    int ch, i, cha;
     char buffer[2000];
 
     FILE *fp;
 
     // initialize curses
     initscr();
+    //cbreak();
     keypad(stdscr, TRUE);
     raw();
+    //scrollok(stdscr, TRUE);
+    noecho();
 
-    getmaxyx(stdscr,maxy,maxx);		// get the number of rows and columns
+    //setlocale(LC_ALL, ""); //for reading non-ascii
+
+    getmaxyx(stdscr,maxy,maxx);	// get the number of rows and columns
     attron(A_STANDOUT);
     for (i = 0;  i < maxx; i++)
         printw(" ");
     mvprintw(0, maxx/2 -10,"Yocto 0.8-alpha1");
     attroff(A_STANDOUT);
-    wmove(stdscr,1, 0);
-
+    move(1, 0);
+/*
     ////READING///
-    /*
     fp = fopen("main.c","r");
-    crow = row;
-    while((ch = fgetc(fp)) != EOF)	// read the file till we reach the end 
+    while((cha = fgetc(fp)) != EOF)	// read the file till we reach the end 
     {
         getyx(stdscr, y, x);		// get the current curser position
-        if(y == (row - 1))			// are we are at the end of the screen 
-        {
-            crow
-            printw("<-Press Any Key->");	// tell the user to press a key 
-            wgetch(stdscr);
-            wmove(stdscr, crow/, 0);			// start at the beginning of the screen 
-
-        }
-        printw("%c", ch);
+        printw("%c", cha);
     }
-    */
-    ////END//OF//READING////
+    ////END//OF//READING////*/
     getmaxyx(stdscr,maxy,maxx);
     while((ch = getch())) {
         if(ch == KEY_DOWN) {
             getyx(stdscr,y,x);
-            wmove(stdscr,y+1, x);
+            if(y == (maxy - 1))	{ // are we are at the end of the screen
+                move(1,0);
+                clrtoeol();
+                move(1, x);
+            }
+            move(y+1, x);
         }
-        if(ch == KEY_UP) {
+        else if(ch == KEY_UP) {
             getyx(stdscr,y,x);
-            wmove(stdscr,y-1, x);
+            if(y!=1)
+                move(y-1, x);
         }
-        if(ch == KEY_LEFT) {
+        else if(ch == KEY_LEFT) {
             getyx(stdscr,y,x);
-            wmove(stdscr,y, x-1);
+            move(y, x-1);
         }
-        if(ch == KEY_RIGHT) {
+        else if(ch == KEY_RIGHT) {
             getyx(stdscr,y,x);
-            wmove(stdscr,y, x+1);
+            move(y, x+1);
         }
-        if(ch == KEY_BACKSPACE) {
+        else if(ch == KEY_BACKSPACE) {
             getyx(stdscr,y,x);
-            if(x == 0)
-                wmove(stdscr,y-1,maxx);
+            move(y,x-1);
             delch();
         }
-        if(ch == KEY_DC) {
-            getyx(stdscr,x,y);
-            wmove(stdscr,y, x+1);
+        else if(ch == KEY_DC) {
             delch();
-            wmove(stdscr,y, x-1);
         }
-        if(ch == 10) { //enter
+        else if(ch == 10) { //enter
             getyx(stdscr,y,x);
-            wmove(stdscr,y+1,0);
+            move(y+1,0);
         }
-        if(ch == ctrl('R')) {
-            wmove(stdscr,1,0);
+        else if(ch == ctrl('R')) {
+            move(1,0);
         }
-        if(ch == ctrl('A')) {
+        else if(ch == ctrl('A')) {
             getyx(stdscr,y,x);
-            wmove(stdscr,y,0);
+            move(y,0);
         }
-        if(ch == ctrl('E')) {
+        else if(ch == ctrl('E')) {
             getyx(stdscr,y,x);
-            wmove(stdscr,y,maxx);
+            getmaxyx(stdscr,maxy,maxx);
+            maxx--;
+	        move(y, maxx);
+            while((inch() &  A_CHARTEXT) == ' ') {
+               move(y, --maxx);
+            }
+            move(y,++maxx);
         }
-        if(ch == ctrl('S')) {
+        else if(ch == ctrl('S')) {
             mvscanw(1,maxx,"%s",&buffer);
             fp = fopen("text.txt","w");
             fprintf(fp, "%s", buffer);
@@ -97,13 +101,13 @@ int main() {
             attron(A_STANDOUT);
             mvprintw(0, maxx -6,"Saved ");
             attroff(A_STANDOUT);
-	        wmove(stdscr,y,x);
-
+	        move(y,x);
         }
-        if(ch == ctrl('C')) {
+        else if(ch == ctrl('C')) {
             endwin();
             return 0;
         }
+        else printw("%c",ch);
     }
     endwin();
     return 0;
