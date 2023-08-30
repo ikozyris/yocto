bab48596bba6a800f86a0bf639a4d29b9ccca34e
ikozyris <80053394+ikozyris@users.noreply.github.com>
2023-08-30 13:10:00 +0300

    Customizable keybindings

---
+++ keybindings.h
@@ -0,0 +1,18 @@
+#define ctrl(x)         ((x) & 0x1f)
+
+#undef 	KEY_ENTER
+#define KEY_ENTER       10 // overwrite default
+
+/* EDIT BELOW to change keybindings*/
+
+#define ENTER		KEY_ENTER
+#define SAVE		ctrl('S')
+#define EXIT		ctrl('C')
+#define UP		KEY_UP
+#define DOWN		KEY_DOWN
+#define LEFT		KEY_LEFT
+#define RIGHT		KEY_RIGHT
+#define BACKSPACE	KEY_BACKSPACE
+#define DELETE		KEY_DC
+#define HOME		ctrl('A')
+#define END		ctrl('E')
