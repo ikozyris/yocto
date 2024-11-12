ikozyris/yocto

commit b90c30992164074bef1b263ae3ada0225b4fbb10

cleanup: highlighting, clarify clearline macro name

main.cpp
@@ -73,8 +73,7 @@ loop:
 		ry = y + ofy;
 		// if out of bounds: move (to avoid bugs) & TODO: simplify
 		if (x >= min(it->len - 1 - ofx, maxx))
-			wmove(text_win, y, min(it->len - ofx - 1, maxx));
-		getyx(text_win, y, x);
+			wmove(text_win, y, x = min(it->len - ofx - 1, maxx));
 		rx = x + ofx;
 		mv_curs(*it, rx);
 
@@ -82,7 +81,7 @@ loop:
 		stats();
 #endif
 #ifdef DEBUG
-		print_text(y); // debug only
+		print_text(y);
 		wmove(text_win, y, x);
 #endif
 	//goto stop;
@@ -257,7 +256,7 @@ loop:
 			if (x == maxx - 1) { // wrap line
 				wrap.push_back(maxx - 1);
 				rx = ofx += maxx - 1;
-				clrln;
+				clearline;
 				mvprint_line(y, 1, *it, ofx, 0);
 				wmove(text_win, y, x = 0);
 			} if (at(*it, rx) == '\t') {

utils/highlight.c
@@ -1,5 +1,6 @@
 #include "init.c"
 
+#define highlight {if (HIGHLIGHT) apply(y);}
 bool eligible;
 
 bool isc(const char *str)

utils/io.cpp
@@ -35,7 +35,7 @@ char *input_header(const char *q)
 	return tmp;
 }
 
-#define clrln (wmove(text_win, y, 0), wclrtoeol(text_win))
+#define clearline (wmove(text_win, y, 0), wclrtoeol(text_win))
 #define mvprint_line(y, x, buffer, from, to) (wmove(text_win, y,x), print_line(buffer, from, to))
 
 // prints substring of buffer, if (to == 0) print until maxx
@@ -71,9 +71,7 @@ void print_text(unsigned line)
 	wmove(text_win, line, 0);
 	for (unsigned ty = line; ty < min(curnum + ofy + 1, maxy) && iter != text.end(); ++iter) {
 		mvprint_line(ty++, 0, *iter, 0, 0);
-#ifdef HIGHLIGHT
-		apply(ty);
-#endif
+		highlight;
 	}
 }
 

utils/key_func.cpp
@@ -148,7 +148,7 @@ void eol()
 			ofx += (long)wrap.back();
 			bytes += nbytes;
 		}
-		clrln;
+		clearline;
 		print_line(*it, bytes, it->len);
 	}
 }
@@ -156,11 +156,9 @@ void eol()
 void sol()
 {
 	if (!wrap.empty()) { // line has been wrapped
-		clrln;
+		clearline;
 		print_line(*it);
-#ifdef HIGHLIGHT
-		apply(y);
-#endif
+		highlight;
 	}
 	wrap.clear();
 	wmove(text_win, y, ofx = 0);
@@ -174,9 +172,7 @@ void scrolldown()
 	mvwprintw(ln_win, maxy - 1, 0, "%3u", ry + 2);
 	wrefresh(ln_win);
 	mvprint_line(y, 0, *it, 0, 0);
-#ifdef HIGHLIGHT
-	apply(y);
-#endif
+	highlight;
 	wmove(text_win, y, 0);
 	wrap.clear();
 	ofx = 0;
@@ -190,9 +186,7 @@ void scrollup()
 	--ofy;
 	--it;
 	mvprint_line(0, 0, *it, 0, 0);
-#ifdef HIGHLIGHT
-	apply(y);
-#endif
+	highlight;
 	wmove(text_win, 0, x);
 	wrefresh(ln_win);
 	wrap.clear();
@@ -203,13 +197,11 @@ void scrollup()
 void left()
 {
 	if (x == 0 && !wrap.empty()) { // line has been wrapped
-		clrln;
+		clearline;
 		ofx -= wrap.back();
 		wrap.pop_back();
 		print_line(*it, ofx < 0 ? 0 : ofx);
-#ifdef HIGHLIGHT
-		apply(y);
-#endif
+		highlight;
 		wmove(text_win, y, maxx - 1);
 	} else if (x > 0) {
 		if (it->buffer[it->gps - 1] == '\t') {
@@ -244,7 +236,7 @@ void right()
 		ofx = 0;
 	} else if (x == maxx - 1) { // right to cut part of line
 wrap_line:
-		clrln;
+		clearline;
 		ofx += x;
 		wrap.push_back(x);
 		print_line(*it, ofx);

commit 11cf9bb08093ec0ed72197542db9b7c7da307dfb

fix: syntax highlighting, cleanup: use namespace std by default
There was a regression from b90c30992164074bef1b263ae3ada0225b4fbb10

headers/gapbuffer.hpp
@@ -63,7 +63,7 @@ void resize(gap_buf &a, unsigned size)
 void mv_curs(gap_buf &a, unsigned pos)
 {
 	if (a.gps == a.gpe) [[unlikely]]
-		resize(a, std::__bit_ceil(a.cpt));
+		resize(a, __bit_ceil(a.cpt));
 	if (pos > a.gps) // move gap to right
 		memmove(a.buffer + a.gps, a.buffer + a.gpe + 1, pos - a.gps);
 	else if (pos < a.gps) // move gap to left
@@ -78,7 +78,7 @@ void mv_curs(gap_buf &a, unsigned pos)
 void insert_c(gap_buf &a, unsigned pos, char ch)
 {
 	if (a.len == a.cpt) [[unlikely]]
-		resize(a, std::__bit_ceil(a.cpt));
+		resize(a, __bit_ceil(a.cpt));
 	if (ingap(a, pos)) { [[likely]]
 		a[pos] = ch;
 		++a.gps;
@@ -93,7 +93,7 @@ void insert_s(gap_buf &a, unsigned pos, const char *str, unsigned len)
 {
 	// is this uneeded? (later it is also checked indirectly)
 	if (a.len + len >= a.cpt) [[unlikely]]
-		resize(a, std::__bit_ceil(a.len + len + 2));
+		resize(a, __bit_ceil(a.len + len + 2));
 	if (gaplen(a) <= len)
 		mv_curs(a, pos);
 	memcpy(a.buffer + pos, str, len);
@@ -104,7 +104,7 @@ void insert_s(gap_buf &a, unsigned pos, const char *str, unsigned len)
 void apnd_c(gap_buf &a, char ch)
 {
 	if (a.len >= a.cpt) [[unlikely]]
-		resize(a, std::__bit_ceil(a.cpt));
+		resize(a, __bit_ceil(a.cpt));
 	a[a.len] = ch;
 	++a.len;
 	++a.gps;
@@ -113,7 +113,7 @@ void apnd_c(gap_buf &a, char ch)
 void apnd_s(gap_buf &a, const char *str, unsigned size)
 {
 	if (a.len + size >= a.cpt) [[unlikely]]
-		resize(a, std::__bit_ceil(a.len + size + 2));
+		resize(a, __bit_ceil(a.len + size + 2));
 	memcpy(a.buffer + a.len, str, size);
 	a.len += size;
 	a.gps = a.len;

headers/headers.h
@@ -7,3 +7,4 @@
 #include <deque>
 #include <bit>
 #include "funcdecl.h"
+using namespace std;

headers/vars.hpp
@@ -8,14 +8,14 @@
 #endif
 unsigned txt_cpt = DEFAULT_LINES; // alloc'ed nodes
 
-std::list<gap_buf> text(DEFAULT_LINES);
-std::list<gap_buf>::iterator it;
+list<gap_buf> text(DEFAULT_LINES);
+list<gap_buf>::iterator it;
 
 // displayed characters of previous wrap (or cut|slice of line)
 #define dchar first
 // bytes printed in current wrap
 #define byte second
-std::deque<std::pair<unsigned, unsigned>> wrap;
+deque<pair<unsigned, unsigned>> wrap;
 
 WINDOW *header_win, *ln_win, *text_win;
 wchar_t s[4];

main.cpp
@@ -144,7 +144,7 @@ loop:
 				} else
 					mvwdelch(text_win, y, x - 1);
 			} else if (y != 0) { // x = 0; merge lines
-				std::list<gap_buf>::iterator curln = it;
+				list<gap_buf>::iterator curln = it;
 				--it;
 				mv_curs(*it, it->len); // delete \n
 				it->gpe = it->cpt - 1;
@@ -162,7 +162,7 @@ loop:
 			if (rx + 1u > it->len)
 				break;
 			if (it->buffer[it->gpe + 1u] == '\n') { // similar to backspace
-				std::list<gap_buf>::iterator curln = it; // current line
+				list<gap_buf>::iterator curln = it; // current line
 				curln->gpe = curln->cpt - 1; // delete newline
 				curln->len--;
 				++it; // next line
@@ -208,7 +208,7 @@ loop:
 			else if (ch == SWITCH) { // switch file
 				argc = 2;
 				argv[1] = input_header("File to open: ");
-				std::list<gap_buf>::iterator iter;
+				list<gap_buf>::iterator iter;
 				unsigned i;
 				for (iter = text.begin(), i = 0; iter != text.end() && i <= curnum; ++iter, ++i) {
 					iter->len = iter->gps = 0;

utils/highlight.c
@@ -1,6 +1,6 @@
 #include "init.c"
 
-#define highlight {if (HIGHLIGHT) apply(y);}
+#define highlight(y) {if (HIGHLIGHT) apply(y);}
 bool eligible;
 
 bool isc(const char *str)

utils/io.cpp
@@ -63,14 +63,14 @@ unsigned print_line(const gap_buf &buffer, unsigned from = 0, unsigned to = 0)
 
 void print_text(unsigned line)
 {
-	std::list<gap_buf>::iterator iter = text.begin();
-	std::advance(iter, ofy + line);
+	list<gap_buf>::iterator iter = text.begin();
+	advance(iter, ofy + line);
 	wmove(text_win, line, 0);
 	wclrtobot(text_win);
 	wmove(text_win, line, 0);
-	for (unsigned ty = line; ty < min(curnum + ofy + 1, maxy) && iter != text.end(); ++iter) {
-		mvprint_line(ty++, 0, *iter, 0, 0);
-		highlight;
+	for (unsigned ty = line; ty < min(curnum + ofy + 1, maxy) && iter != text.end(); ++iter, ++ty) {
+		mvprint_line(ty, 0, *iter, 0, 0);
+		highlight(ty);
 	}
 }
 
@@ -82,7 +82,7 @@ void save()
 		filename = (char*)input_header("Enter filename: ");
 	FILE *fo = fopen(filename, "w");
 	unsigned i;
-	std::list<gap_buf>::iterator iter;
+	list<gap_buf>::iterator iter;
 	for (iter = text.begin(), i = 0; iter != text.end() && i <= curnum; ++iter, ++i) {
 		data(*iter, 0, iter->len);
 		fputs(lnbuf, fo);

utils/key_func.cpp
@@ -102,7 +102,7 @@ void command()
 			print_lines();
 			wrefresh(ln_win);
 			print_text(0);
-			std::advance(it, ofy - ry);
+			advance(it, ofy - ry);
 		}
 	} else
 		print2header("command not found", 3);
@@ -168,7 +168,7 @@ void sol()
 	if (!wrap.empty()) { // line has been wrapped
 		clearline;
 		print_line(*it);
-		highlight;
+		highlight(y);
 	}
 	wrap.clear();
 	wmove(text_win, y, ofx = 0);
@@ -182,7 +182,7 @@ void scrolldown()
 	mvwprintw(ln_win, maxy - 1, 0, "%3u", ry + 2);
 	wrefresh(ln_win);
 	mvprint_line(y, 0, *it, 0, 0);
-	highlight;
+	highlight(y);
 	wmove(text_win, y, 0);
 	wrap.clear();
 	ofx = 0;
@@ -196,7 +196,7 @@ void scrollup()
 	--ofy;
 	--it;
 	mvprint_line(0, 0, *it, 0, 0);
-	highlight;
+	highlight(y);
 	wmove(text_win, 0, x);
 	wrefresh(ln_win);
 	wrap.clear();
@@ -210,7 +210,7 @@ void left()
 		ofx -= wrap.back().dchar;
 		wrap.pop_back();
 		print_line(*it, wrap.empty() ? 0 : wrap.back().byte);
-		highlight;
+		highlight(y);
 		wmove(text_win, y, flag + 1);
 	} else if (x > 0) {
 		wmove(text_win, y, x - 1);